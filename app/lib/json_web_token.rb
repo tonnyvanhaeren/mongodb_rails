849a005851426440250b0a8fe2f02efe88078c00
Vanhaeren <antonius.vanhaeren@telenet.be>
2020-06-19 08:47:31 +0200

    rspec jsonwebtoken encode decode expiration

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,13 +2,16 @@
 class JsonWebToken
   class << self
     def encode(payload, exp = 1.hour.from_now)
-      payload[:expiration] = exp.to_i
+      payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.credentials.jwt[:secret])
     end
  
     def decode(token)
       body = JWT.decode(token, Rails.application.credentials.jwt[:secret])[0]
       HashWithIndifferentAccess.new body
+    rescue JWT::ExpiredSignature
+      #handle expiration
+      raise JWT::ExpiredSignature
     rescue
       nil
     end
--- spec/lib/json_webtoken/json_web_token_spec.rb
+++ spec/lib/json_webtoken/json_web_token_spec.rb
@@ -2,30 +2,41 @@ require 'rails_helper'
 
 describe JsonWebToken do
   let(:andries) { build :user, email: 'bob.andries@telenet.be', lastName: 'Andries', password: '1Telindus' }
-  
-  subject { described_class.encode( id: andries._id, email: andries.email, role: andries.role ) }
-  let(:token) { subject }
 
   after(:context) do
     User.delete_all 
   end ### clean database  
-  
-  describe '#encode and decode jwt token' do
+
+  context '#encode - decode not expired' do
+    subject { described_class.encode( { id: andries._id, email: andries.email, role: andries.role } ) }
+    let(:token) { subject }
+
     it 'return a jwt token' do
       expect(subject).to eq(token)
     end
 
-    it 'includes in payload expiration email role id' do
+    it 'includes in payload exp email role id' do
       expect(described_class.decode(token)).to include(
         {
-          expiration: described_class.decode(token)[:expiration],
+          exp: described_class.decode(token)[:exp],
           email: 'bob.andries@telenet.be',
           role: 'user'
         }
       )
-    end
-
-
+    end    
   end
 
+  context '#encode - decode (token expired)' do
+    subject { described_class.encode( { id: andries._id, email: andries.email, role: andries.role }, Time.now.ago(6000) ) }
+    let(:token) { subject }
+    
+    it 'return a jwt token' do
+      expect(subject).to eq(token)
+    end
+
+    it 'raise Jwt Expired Signature error' do
+      expect { described_class.decode(token) }.to raise_error(JWT::ExpiredSignature)
+    end    
+  end
+    ## exp: Time.now.to_i + 4 * 3600
 end
